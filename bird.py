308b46425f25b29bc8d89d496f7a0580d211b45a
Елизавета Буторина <155951551+Uelizaveta3@users.noreply.github.com>
2026-05-16 18:40:22 +0500

    Update bird.py

--- bird.py
+++ bird.py
@@ -1,6 +1,14 @@
 import spritePro as s
 
-bg = s.Sprite('background_bird.png',pos=(200,300),size=(400,600))
-bird = s.Sprite('bird.png',pos=(100,300),size=(50,50))
 
-s.run(size=(400,600), title="Flappy Bird")
+class GameScene(s.Scene):
+    def __init__ (self):
+    super().__init__()
+        bg = s.Sprite('background_bird.png',pos=(200,300),size=(400,600))
+        bird = s.Sprite('bird.png',pos=(100,300),size=(50,50))
+        self.player_body = s.add_physics(self.bird, s.PhysicsConfig(bounce=0.0))
+        s.physics.set_gravity(980)
+        s.physics.set_bounds(s.pygame.Rect(0,0,400,600))
+
+if __name__ == '__main__':
+    s.run(size=(400,600), title="Flappy Bird")
